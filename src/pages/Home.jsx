89c0a92729e8a7d9272a502abaa2fa3b3b42e025
Elle Ordoña <eto0507@hotmail.co.uk>
2025-07-17 13:11:11 +0100

    main content: skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "../components/AboutSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -15,11 +16,14 @@ export const Home = () => {
 
             {/* NavBar */}
             <Navbar />
+            //! Need to fix the mobile navbar
+            //TODO: Adjust the Home href to path to the hero section
 
             {/* Main Content */}
             <main>
                 <HeroSection />
                 <AboutSection />
+                <SkillsSection />
             </main>
 
             {/* Footer */}
